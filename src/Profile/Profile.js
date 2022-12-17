ac70e70dce846f9625be3900f39df6aceea7689c
Irwing Tello <28800239+irwingtello@users.noreply.github.com>
2022-12-17 05:27:14 -0600

    Internal transaction - Open Zepellin roles

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,72 +1,267 @@
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { useAccount, useConnect, useDisconnect,usePrepareContractWrite, useContractWrite ,useSendTransaction,useWaitForTransaction  } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import * as React from 'react'
+import  {useState } from 'react';
+import Box from '@mui/material/Box'
 import { useDebounce } from 'use-debounce'
-import { usePrepareSendTransaction, useSendTransaction,useWaitForTransaction  } from 'wagmi'
-import { utils } from 'ethers'
+import { ethers } from "ethers";
+import InputLabel from '@mui/material/InputLabel';
+import TextField from '@mui/material/TextField';
+import FormControl from "@mui/material/FormControl";
+import FormLabel from '@mui/material/FormLabel';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import { NFTStorage } from "nft.storage/dist/bundle.esm.min.js";
+import { useSignMessage } from 'wagmi'
+import domainsAbi from "../Resources/DomainsABI.json";
+var connected = false;
 function Profile() {
-  const { address, isConnected } = useAccount()
-  const { connect } = useConnect({
-    connector: new InjectedConnector(),
-  })
+  const Web3 = require('web3');
+  const { Wallet, Contract } = require('ethers');
+  const { createCanvas } = require('canvas');
+  const client= new NFTStorage({token:process.env.REACT_APP_NFT_API_KEY});
   const { disconnect } = useDisconnect()
 
-  const [to, setTo] = React.useState('')
-  const [debouncedTo] = useDebounce(to, 500)
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [userBalance, setUserBalance] = useState(null);
+  
+  const [addressField, setAddressField] = React.useState('')
+  const [addressFieldError, setAddressFieldError] = useState();
 
-  const [amount, setAmount] = React.useState('')
-  const [debouncedAmount] = useDebounce(amount, 500)
+  const [textField, setTextField] = React.useState('')
+  const [textFieldError, setTextFieldError] = useState();
 
-  const { config } = usePrepareSendTransaction({
-    request: {
-      to: debouncedTo,
-      value: debouncedAmount ? utils.parseEther(debouncedAmount) : undefined,
-    },
-  })
-  const { data, sendTransaction } = useSendTransaction(config)
-  const { isLoading, isSuccess } = useWaitForTransaction({
-    hash: data?.hash,
-  })
+  const [pohField, setPOHField] = React.useState('')
+  const [pohIPFSField, setPOHIPFSField] = React.useState('')
 
-  if (isConnected)
+
+
+  const changeTextField = (event) => {
+    setTextField(event.target.value);
+  };
+  const changeAddressField = (event) => {
+    setAddressField(event.target.value);
+  };
+  const [connButtonText, setConnButtonText] = useState("Connect Wallet");
+  const connectWallet = async (event) => {
+            // Check if the browser supports Ethereum
+          if (window.ethereum) {
+            // Set the provider to be used (in this case, the injected provider from MetaMask)
+            const web3 = new Web3(window.ethereum);
+
+            try {
+              window.ethereum
+              .request({ method: "eth_requestAccounts" })
+              .then((result) => {
+                accountChangedHandler(result[0]);
+                setConnButtonText("Wallet Connected");
+                getAccountBalance(result[0]);
+      
+                connected = true;
+              })
+              .catch((error) => {
+                setErrorMessage(error.message);
+              });
+            } catch (error) {
+              // User denied account access...
+            }
+          }
+          // Legacy dapp browsers...
+          else if (window.web3) {
+            // Set the provider to be used (in this case, the injected provider from MetaMask)
+            const web3 = new Web3(window.web3.currentProvider);
+
+            // Set the default account to be used by web3.js
+            web3.eth.defaultAccount = window.web3.eth.defaultAccount;
+          }
+          // Non-dapp browsers...
+          else {
+            console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+          }
+  };
+  // update account, will cause component re-render
+  const accountChangedHandler = (newAccount) => {
+    setDefaultAccount(newAccount);
+    getAccountBalance(newAccount.toString());
+  };
+
+  const getAccountBalance = (account) => {
+    window.ethereum
+      .request({ method: "eth_getBalance", params: [account, "latest"] })
+      .then((balance) => {
+        setUserBalance(ethers.utils.formatEther(balance));
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  };
+  const chainChangedHandler = () => {
+    // reload the page to avoid any errors with chain change mid use of application
+    window.location.reload();
+  };
+  const check=()=>{
+    window.ethereum.enable()
+    .then(() => {
+      console.log('MetaMask permission granted');
+    })
+    .catch((error) => {
+      console.error('MetaMask permission denied');
+    });
+  }
+  // listen for account changes
+  window.ethereum.on("accountsChanged", accountChangedHandler);
+
+  window.ethereum.on("chainChanged", chainChangedHandler);
+
+  const handleSubmit = async () => {
+    console.log(window.ethereum.selectedAddress);
+    const canvasBackground = createCanvas(900, 800);
+    const cxBackground = canvasBackground.getContext('2d');
+    cxBackground.fillStyle = "#14213d";
+    cxBackground.fillRect(0, 0, 1800,1800);
+    cxBackground.font = '100px Arial';
+    cxBackground.fillStyle = '#e5e5e5';
+    cxBackground.fillText(textField,100,600);
+
+   canvasBackground.toBlob(async function(blob) {
+      const metadata =await client.store({
+        name: 'Proof of Help',
+        description:textField,
+        image: new File([blob], 'proofofhelp.jpg', { type: 'image/jpg' }),
+      })    
+    await fetch(metadata.url.replace("ipfs://", "https://nftstorage.link/ipfs/"))
+    .then(res => res.json())
+    .then((out) => {
+      setPOHIPFSField(out.image);
+      setPOHField(out.image.replace("ipfs://", "https://nftstorage.link/ipfs/"));
+      }).catch(err => console.error(err));
+    });
+
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    /*
+    Only account with role
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const walletAddress = accounts[0]    // first account in MetaMask
+    const gasPayer = provider.getSigner(walletAddress);
+    const contract = new ethers.Contract("0x80aeF98D67B84B265e43a3F86382f70948a18eAE", domainsAbi, gasPayer)
+    const name = await contract.helpRegister("x","x");
+    */
+     // defining the wallet private key
+     // Set the contract address and ABI
+      const web3 = new Web3(window.ethereum);
+     let privatekey = process.env.REACT_APP_SIGNER;
+     const contractAddress = '0x80aeF98D67B84B265e43a3F86382f70948a18eAE';
+     
+     // Set the provider (MetaMask) and signer (private key)
+     const provider = new ethers.providers.Web3Provider(window.ethereum);
+     const wallet = new ethers.Wallet(privatekey, provider);
+     console.log(wallet);
+     //Get the contract instance
+     const contract = new ethers.Contract(contractAddress, domainsAbi, wallet);
+     
+     //Metamask
+     const signer = provider.getSigner();
+     const contractMetamask = new ethers.Contract(contractAddress, domainsAbi, signer);
+
+     // Set the parameters for the function call
+     const name = 'John';
+     const tokenURI = 'https://example.com/token/123';
+     const nonce=await provider.getTransactionCount(wallet.address);
+     console.log("nonce: " + nonce);
+   
+
+
+      const nonceMetamask=await provider.getTransactionCount(accounts[0]);
+    
+      // Populate the transaction object
+ 
+      const tx = await contract.populateTransaction.helpRegister(name, tokenURI, {
+         nonce: nonce,
+         gasLimit: 3000000
+      });
+      
+       const amountxx=await getTotalGasCostInWei(tx);
+    
+      /*
+       const txx = await contract.helpRegister(name, tokenURI, { gasLimit: 3000000});
+                // Wait for the transaction to be mined
+        await txx.wait();
+      */
+
+
+  }
+  async function getTotalGasCostInWei(tx) {
+    try {
+      const web3 = new Web3(window.ethereum);
+      const gasPrice = await web3.eth.getGasPrice();
+      const gasEstimate = await web3.eth.estimateGas(tx);
+      const gasEstimatePlus= gasEstimate * 1.7;
+      console.log(gasEstimatePlus * gasPrice);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  if (connected)
     return (
       <div>
-        Connected to {address}
-                <form
-                onSubmit={(e) => {
-                    e.preventDefault()
-                    sendTransaction?.()
-                }}
-                >
-                <input
-                    aria-label="Recipient"
-                    onChange={(e) => setTo(e.target.value)}
-                    placeholder="0xA0Cf…251e"
-                    value={to}
-                />
-                <input
-                    aria-label="Amount (filecoin)"
-                    onChange={(e) => setAmount(e.target.value)}
-                    placeholder="0.05"
-                    value={amount}
-                />
-                  <button disabled={isLoading || !sendTransaction || !to || !amount}>
-                    {isLoading ? 'Sending...' : 'Send'}
-                     </button>
-                    {isSuccess && (
-                        <div>
-                        Successfully sent {amount} Filecoin to {to}
-                        <div>
-                            <a href={`https://explorer.glif.io/tx/${data?.hash}/?network=wallabynet`}>Glif Scan</a>
-                        </div>
-                        </div>
-                    )}
+         <Box
+        component="form"
+         noValidate
+            sx={{
+              my: 4,
+              mx: 12,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+            
+          >  
+          <FormControl >
+              <center>
+                  <FormLabel id="address" >{defaultAccount}</FormLabel>
+                  <TextField
+                              InputLabelProps={{ shrink: true }}
+                              error= {addressFieldError? true:false}
+                              margin="normal"
+                              fullWidth
+                              id="name"
+                              label="Address Name"
+                              name="name"
+                              value={addressField}
+                              onChange={changeAddressField}
+                                              
+                      />  
+                      <TextField
+                              InputLabelProps={{ shrink: true }}
+                              error= {textFieldError? true:false}
+                              margin="normal"
+                              fullWidth
+                              id="text"
+                              label="Text"
+                              name="text"
+                              value={textField}
+                              onChange={changeTextField}
+                                              
+                      />  
+                <FormLabel sx={{m:3}} id="mintedPOH" ><Link href={pohField}>{pohField}</Link></FormLabel>
+                <br/>
+                <br/>
+                <Button onClick={() => handleSubmit()} variant="contained" >
+                  Mint
+                </Button>
+                <Button onClick={() =>check()} variant="contained" >
+                  Wallet permission check
+                </Button>
+                </center>
+            </FormControl>
+           </Box>
 
-                </form>
-        <button onClick={() => disconnect()}>Disconnect</button>
       </div>
     )
-  return <button onClick={() => connect()}>Connect Wallet</button>
+  return  <Button onClick={() => connectWallet()}  sx={{m: 2}} variant="contained" color="success">Connect Wallet</Button>
 }
 
 export default Profile;
---
+++ src/Resources/Domains.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import {StringUtils} from "../resources/StringUtils.sol";
+// We import another help function
+import "@openzeppelin/contracts/utils/Base64.sol";
+error Unauthorized();
+error AlreadyRegistered();
+error InvalidName(string name);
+// We inherit the contract we imported. This means we'll have access
+// to the inherited contract's methods.
+contract Domains is ERC721URIStorage, AccessControl {
+    // Add this at the top of your contract next to the other mappings
+    bytes32 public constant APP_ROLE = keccak256("APP_ROLE");
+    mapping (uint => string) public names;
+    mapping (uint => uint) public domainPrice;
+    mapping(string => address) public domains;
+
+
+    constructor() ERC721 ("Proof of Help", "POH") payable {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(APP_ROLE, msg.sender);
+        owner = payable(msg.sender);
+        tld = "poh";
+        domainPrice[0]=0.5 * 10 **17;
+        domainPrice[1]=0.3 * 10 **17;
+        domainPrice[2]=0.1 * 10 **17;
+      }
+     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    Counters.Counter private _helpTokenId;
+
+    string public tld;
+    address payable public owner;
+
+    function getAllNames() public view returns (string[] memory) {
+        string[] memory allNames = new string[](_tokenIds.current());
+        for (uint i = 0; i < _tokenIds.current(); i++) {
+            allNames[i] = names[i];
+        }
+        return allNames;
+    }
+
+    function valid(string calldata _name) public pure returns(bool) {
+      return StringUtils.strlen(_name) >= 3 && StringUtils.strlen(_name) <= 30;
+    }
+
+    function withdraw() public  onlyRole(DEFAULT_ADMIN_ROLE) {
+    uint amount = address(this).balance;
+    
+    (bool success, ) = msg.sender.call{value: amount}("");
+    require(success, "Failed to withdraw Filecoin");
+    } 
+
+    function register(string calldata _name,string calldata _tokenURI) public payable onlyRole(APP_ROLE) {
+      //require(domains[name] == address(0));
+      if (domains[_name] != address(0)) revert AlreadyRegistered();
+      if (!valid(_name)) revert InvalidName(_name);
+      uint256 _price = price(_name);
+
+      require(msg.value >= _price, "Not enough Filecoin paid");
+      
+      uint256 newRecordId = _tokenIds.current();
+
+      // Combine the name passed into the function  with the TLD
+      string memory name = string(abi.encodePacked(_name, ".", tld));
+
+      _safeMint(msg.sender, newRecordId);
+      _setTokenURI(newRecordId, _tokenURI);
+      domains[name] = msg.sender;
+      names[newRecordId] = name;
+      _tokenIds.increment();
+    }
+
+   function helpRegister(string calldata _name,string calldata _tokenURI) public  onlyRole(APP_ROLE) {
+      _helpTokenId.increment();
+    }
+
+  
+    // This function will give us the price of a domain based on length
+    function price(string calldata _name) public view returns(uint) {
+      uint len = StringUtils.strlen(_name);
+      require(len > 0);
+      if (len == 3) {
+        return domainPrice[0]; 
+      } else if (len == 4) {
+        return domainPrice[1]; 
+      } else {
+        return domainPrice[2];
+      }
+    }
+
+    function setAddress(address _address,string calldata _name) public onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+      domains[_name] = _address;
+    }
+    
+    function getAddress(string calldata _name) public view returns (address) {
+        return domains[_name];
+    }
+
+    function changePrice(uint _index, uint _price) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        domainPrice[_index] = _price;
+    }
+
+
+}
