f1a00553e16a4e30ad49585a1dde179cb25555f8
samuelpolino97 <polinosamuel1997@outlook.com>
2023-03-15 08:51:00 -0600

    correct design

--- src/page/Cards.js
+++ src/page/Cards.js
@@ -1,57 +1,65 @@
 import React from "react";
 import "./Home.css";
-import { Box } from "@mui/system";
+
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const Cards = () => {
   return (
     <div className="container">
-      <div className="card , izquierda">
-        <div class="header"></div>
-        <div class="info">
-          <p class="title">How to make this material card ?</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi.{" "}
-          </p>
-        </div>
-        <div class="footer">
-          <button type="button" class="action">
-            Get started{" "}
-          </button>
-        </div>
-      </div>
+      <Card className="left" sx={{ maxWidth: 340 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </Card>
 
-      <div className="card , centro">
-        <div class="header"></div>
-        <div class="info">
-          <p class="title">How to make this material card ?</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi.{" "}
-          </p>
-        </div>
-        <div class="footer">
-          <button type="button" class="action">
-            Get started{" "}
-          </button>
-        </div>
-      </div>
-
-      <div className="card , derecha">
-        <div class="header"></div>
-        <div class="info">
-          <p class="title">How to make this material card ?</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi.{" "}
-          </p>
-        </div>
-        <div class="footer">
-          <button type="button" class="action">
-            Get started{" "}
-          </button>
-        </div>
-      </div>
+      <Card className="center" sx={{ maxWidth: 340 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card className=" right" sx={{ maxWidth: 340 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 };
